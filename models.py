ff8cd8ff3d22fc80b4c6a4233ca97ce111cc55db
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-25 16:51:46 -0800

    added posts

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -17,3 +18,14 @@ class User(db.Model):
     
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    __tablename__ = "posts"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, server_default = func.now())
+    creator_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    
+    creator = db.relationship("User", backref="posts")
