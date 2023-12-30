72158694b21bc8c399986bbc66273de621a807da
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-30 03:13:12 -0800

    added tags to model

--- models.py
+++ models.py
@@ -29,3 +29,16 @@ class Post(db.Model):
     creator_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     
     creator = db.relationship("User", backref="posts")
+    tags = db.relationship("Tag", secondary="posts_tags", backref="posts")
+    
+class Tag(db.Model):
+    __tablename__ = "tags"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+class PostTag(db.Model):
+    __tablename__ = "posts_tags"
+    
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
