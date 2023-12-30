a118a1f77b2a8318ca09f200ddcae4c7cf4d8c1c
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-30 03:13:25 -0800

    updated seed with more posts and to include tags

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import User, Post, db
+from models import User, Post, Tag, db
 from app import app
 
 # need to do this in the current version of SQLAlchemy
@@ -15,13 +15,35 @@ users = [
     User(first_name = "Samantha", last_name="Myers"),
 ]
 
+tags = [
+    Tag(name="Introductions"),
+    Tag(name="Life Updates"),
+    Tag(name="Musings"),
+    Tag(name="Poetry"),
+    Tag(name="Fun"),
+    Tag(name="Flask"),
+    Tag(name="Programming"),
+]
+
 posts = [
-    Post(title="First Post!", content="w00t!", creator_id=2),
-    Post(title="On Emptiness", content=" ", creator_id=4),
-    
+    Post(title="First Post!", creator_id=2, content="w00t!",
+         tags=[tags[0], tags[4]]),
+    Post(title="On Emptiness", creator_id=4, content=" ",
+         tags=[tags[2], tags[3]]),
+    Post(title="The beginning", creator_id=1, content="I'm excited to share my blogging journey with all of you",
+         tags=[tags[0], tags[1], tags[2]]),
+    Post(title="Flask is great", creator_id=3, content="Flask is an amazing tool for creating websites",
+         tags=[tags[5], tags[6]]),
+    Post(title="Um, hi", creator_id=5, content="hello! or not! sorry for bothering you",
+         tags=[tags[0]]),
+    Post(title="What I had for breakfast today", creator_id=1, content="Hello dear subscribers! Today for breakfast I had an omelet. Stay tuned for more exciting updates",
+         tags=[tags[4], tags[1]])
 ]
 
 db.session.add_all(users)
+db.session.commit()
+
+db.session.add_all(tags)
 db.session.commit()
 
 db.session.add_all(posts)
